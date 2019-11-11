80ab41f399831968ab4e970ba12cadae381dd501
srinivassankoji <srinivaschary.chary@gmail.com>
2019-11-11 13:22:43 +0530

    Adding Project

---
+++ DefaultReaderWriter/src/main/java/com/example/demo/DefaultReaderWriterApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefaultReaderWriterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefaultReaderWriterApplication.class, args);
+	}
+
+}
